241300b696e79dd1e3ce2a20d492489798ef5991
Jerry Huang <jerry83314@gmail.com>
2023-12-11 23:35:20 +0800

    撰寫 ts

--- task.js
+++ task.js
@@ -10,3 +10,28 @@ var TaskStatus;
 ;
 // 空陣列
 let tasks = [];
+// 點擊增加任務按鈕
+document.querySelector('.addTask').addEventListener('click', () => {
+    const taskNameInput = document.querySelector('.taskName');
+    const taskContent = taskNameInput.value;
+    const taskItem = {
+        name: taskContent,
+        status: TaskStatus.Todo
+    };
+    createTask(taskItem);
+});
+// 新增陣列
+function createTask(task) {
+    tasks.push(task);
+    updateTaskList();
+}
+;
+// 渲染 list
+function updateTaskList() {
+    let taskContent = '';
+    tasks.forEach(task => {
+        taskContent += `<li>${task.name} - 狀態：${TaskStatus[task.status]}</li>`;
+    });
+    document.querySelector('.taskList').innerHTML = taskContent;
+}
+;
--- task.ts
+++ task.ts
@@ -1,6 +1,7 @@
 // 任務介面型別
 interface Task {
-  name: string
+  name: string,
+  status: TaskStatus
 };
 
 // 任務列舉
@@ -12,3 +13,29 @@ enum TaskStatus {
 
 // 空陣列
 let tasks: Task[] = [];
+
+// 點擊增加任務按鈕
+document.querySelector('.addTask')!.addEventListener('click', () => {
+  const taskNameInput = document.querySelector('.taskName') as HTMLInputElement;
+  const taskContent = taskNameInput.value;
+  const taskItem: Task = {
+    name: taskContent,
+    status: TaskStatus.Todo
+  };
+  createTask(taskItem);
+});
+
+// 新增陣列
+function createTask(task: Task) {
+  tasks.push(task);
+  updateTaskList();
+};
+
+// 渲染 list
+function updateTaskList() {
+  let taskContent = '';
+  tasks.forEach(task => {
+    taskContent += `<li>${task.name} - 狀態：${TaskStatus[task.status]}</li>`
+  });
+  document.querySelector('.taskList')!.innerHTML = taskContent;
+};
